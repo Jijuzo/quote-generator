690020057ab191b9ef2b090776d30f28d13342fc
Ihor Marchuk <usdreldo@gmai.com>
2023-08-08 17:54:09 +0200

    added error and loading handlers, fetch functions simplified

--- js/quote-generator.js
+++ js/quote-generator.js
@@ -1,31 +1,71 @@
 const randomBtn = document.querySelector("#random-btn");
 const allAuthorsBtn = document.querySelector("#all-authors-btn");
+const backToQuotesBtn = document.querySelector("#back-to-quotes-btn");
 const quote = document.querySelector(".quote");
-let quoteAuthor = document.querySelector(".quote-author");
-let quoteGenre = document.querySelector(".quote-genre");
+const quoteAuthor = document.querySelector(".quote-author");
+const quoteGenre = document.querySelector(".quote-genre");
 const allAuthorsContainer = document.querySelector(".all-authors-container");
 const gridContainer = document.querySelector(".grid-container");
 const topButtons = document.querySelector(".top-buttons");
 const footer = document.querySelector("footer");
+const quoteLoader = document.querySelector("#quote-loader");
+const allAuthorsLoader = document.querySelector("#all-authors-loader");
+let allAuthorsFetchCalled = false;
 
 async function getRandomQuote() {
-  const promise = await fetch("https://api.quotable.io/random");
-  const result = await promise.json();
-  quote.textContent = result.content;
-  quoteAuthor.textContent = result.author;
-  quoteGenre.textContent = result.tags.join(", ");
+  try {
+    quoteLoader.style.display = "flex";
+    quote.style.display = "none";
+    quoteAuthor.style.display = "none";
+    quoteGenre.style.display = "none";
+    const promise = await fetch("https://api.quotable.io/random");
+    if (!promise.ok) {
+      throw new Error(`HTTP error! Status: ${promise.status}`);
+    }
+    quoteLoader.style.display = "none";
+    quote.style.display = "block";
+    quoteAuthor.style.display = "block";
+    quoteGenre.style.display = "block";
+    return (result = await promise.json());
+  } catch (error) {
+    console.error("Error:", error);
+    quoteLoader.style.display = "none";
+    throw error;
+  }
 }
 
-randomBtn.addEventListener("click", getRandomQuote);
-getRandomQuote();
+async function setRandomQuote() {
+  const randomQuote = await getRandomQuote();
+  quote.textContent = randomQuote.content;
+  quoteAuthor.textContent = randomQuote.author;
+  quoteGenre.textContent = randomQuote.tags.join(", ");
+}
+
+randomBtn.addEventListener("click", setRandomQuote);
+setRandomQuote();
 
 async function getAllAuthors() {
-  const promise = await fetch(
-    `https://api.quotable.io/authors?sortBy=quoteCount&order=desc&limit=10`
-  );
-  const result = await promise.json();
+  try {
+    allAuthorsLoader.style.display = "flex";
+    const promise = await fetch(
+      `https://api.quotable.io/authors?sortBy=quoteCount&order=desc&limit=10`
+    );
+    if (!promise.ok) {
+      throw new Error(`HTTP error! Status: ${promise.status}`);
+    }
+    allAuthorsLoader.style.display = "none";
+    return (result = await promise.json());
+  } catch (error) {
+    console.error("Error:", error);
+    allAuthorsLoader.style.display = "none";
+    throw error;
+  }
+}
+
+async function setAllAuthors() {
+  const authors = await getAllAuthors();
   let count = 1;
-  for (let oneAuthor of result.results) {
+  for (let oneAuthor of authors.results) {
     const authorContainer = document.createElement("div");
     authorContainer.classList.add("all-authors-item");
     const authorInfo = document.createElement("h3");
@@ -37,19 +77,24 @@ async function getAllAuthors() {
   }
 }
 
-allAuthorsBtn.addEventListener("click", getAllAuthors(), { once: true });
+backToQuotesBtn.style.visibility = "hidden";
 
 allAuthorsBtn.addEventListener("click", () => {
-  allAuthorsBtn.classList.toggle("active");
-  if (allAuthorsBtn.classList.contains("active")) {
-    gridContainer.style.display = "none";
-    topButtons.style.marginBottom = "5vh";
-    allAuthorsBtn.textContent = "go back to quotes";
-    allAuthorsContainer.style.display = "flex";
-  } else {
-    allAuthorsContainer.style.display = "none";
-    topButtons.style.marginBottom = "15vh";
-    allAuthorsBtn.textContent = "all authors";
-    gridContainer.style.display = "grid";
+  if (!allAuthorsFetchCalled) {
+    setAllAuthors();
+    allAuthorsFetchCalled = true;
   }
+  allAuthorsBtn.style.visibility = "hidden";
+  backToQuotesBtn.style.visibility = "visible";
+  gridContainer.style.display = "none";
+  topButtons.style.marginBottom = "5vh";
+  allAuthorsContainer.style.display = "flex";
+});
+
+backToQuotesBtn.addEventListener("click", () => {
+  backToQuotesBtn.style.visibility = "hidden";
+  allAuthorsBtn.style.visibility = "visible";
+  allAuthorsContainer.style.display = "none";
+  topButtons.style.marginBottom = "15vh";
+  gridContainer.style.display = "grid";
 });
